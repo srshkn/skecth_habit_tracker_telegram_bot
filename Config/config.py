524828e312192d47df9470801e640a5feba9503a
srshkn <snuqlstyas@gmail.com>
2025-12-13 06:31:24 +0500

    add Config & Users, updata main

--- Config/config.py
+++ Config/config.py
@@ -1,1 +1,18 @@
-print('config')
+from environs import Env
+from dataclasses import dataclass
+
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(bot=TgBot(token=env("BOT_TOKEN")))
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('main')
+import asyncio
+
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
+
+from Config.config import Config, load_config
+
+
+async def main():
+    config: Config = load_config()
+
+    bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+    dp = Dispatcher()
+
+if __name__ == "__main__":
+    asyncio.run(main())
